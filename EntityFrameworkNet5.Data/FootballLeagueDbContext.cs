81a3e77a32c9c7923362457cd05472124a0102f6
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-09 09:40:14 +0100

    Add properties to represent table rows for both tables

---
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkNet5.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkNet5.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+    }
+}
