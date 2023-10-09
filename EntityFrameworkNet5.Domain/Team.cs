a22d456eb3da52bf6382b6e259a7c7c1dddf6f2e
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-09 09:39:32 +0100

    Make Team model public for access in context

--- EntityFrameworkNet5.Domain/Team.cs
+++ EntityFrameworkNet5.Domain/Team.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkNet5.Domain
 {
-    internal class Team
+    public class Team
     {
         public int Id { get; set; }
         public string Name { get; set; }
