21a11c7514634d9ded2126ea375bcb90a4fd48d7
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-10 09:41:43 +0100

    Add simple insertion

--- EntityFrameworkNet5.ConsoleApp/Program.cs
+++ EntityFrameworkNet5.ConsoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntityFrameworkNet5.Data;
+using EntityFrameworkNet5.Domain;
+using System;
+
+namespace EntityFrameworkNet5.ConsoleApp
+{
+    class Program
+    {
+        private static FootballLeagueDbContext context = new FootballLeagueDbContext();
+
+        static async Task Main(string[] args)
+        {
+            var league = new League { Name = "Bundesliga" };
+            var team = new Team { Name = "Bayern Munich", League = league };
+            await context.AddAsync(team);
+            await context.SaveChangesAsync();
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
+        }
+    }
+}
