c460fe6d0cda223fcff97ac2b1a2b26a41259eb4
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-11 15:54:42 +0100

    Add simple select query

--- EntityFrameworkNet5.ConsoleApp/Program.cs
+++ EntityFrameworkNet5.ConsoleApp/Program.cs
@@ -10,13 +10,78 @@ namespace EntityFrameworkNet5.ConsoleApp
 
         static async Task Main(string[] args)
         {
+            /* Simple Insert Operation Methods */
+            // await AddNewLeague();
+            // await AddNewTeamsWithLeague();
+
+            /* Simple Select Queries */
+            // SimpleSelectAllQuery();
+
+
+            Console.WriteLine("Press any key to end...");
+            Console.ReadKey();
+        }
+
+        static void SimpleSelectAllQuery()
+        {
+            // Get all leagues (SQL: SELECT * FROM Leagues)
+            // Smartest, efficient way 
+            var leagues = context.Leagues.ToList();
+            foreach (var league in leagues)
+            {
+                Console.WriteLine($"{league.Id}: {league.Name}");
+            }
+
+            // Only works with ToList() on the end otherwise won't execute until foreach
+            // Then will keep the connection to the DB open for the time of the foreach loop running (which won't scale)
+            // Might also create a lock on the table
+            //foreach (var league in context.Leagues)
+            //{
+            //    Console.WriteLine($"{league.Id}: {league.Name}");
+            //}
+        }
+
+        static async Task AddNewLeague()
+        {
+            // Adding a new League Object
+            var league = new League { Name = "Serie A" };
+            await context.Leagues.AddAsync(league);
+            await context.SaveChangesAsync();
+
+            // Function to add new teams related to the new League Object
+            await AddTeamsWithLeague(league);
+            await context.SaveChangesAsync();
+        }
+
+        static async Task AddTeamsWithLeague(League league)
+        {
+            var teams = new List<Team>
+        {
+            new Team
+            {
+            Name = "Juventus",
+            LeagueId = league.Id
+            },
+            new Team
+            {
+                Name = "AC Milan",
+                LeagueId = league.Id
+            },
+            new Team
+            {
+                Name = "AS Roma",
+                League = league
+            }
+        };
+            await context.AddRangeAsync(teams);
+        }
+
+        static async Task AddNewTeamsWithLeague()
+        {
             var league = new League { Name = "Bundesliga" };
             var team = new Team { Name = "Bayern Munich", League = league };
             await context.AddAsync(team);
             await context.SaveChangesAsync();
-
-            Console.WriteLine("Press any key to end...");
-            Console.ReadKey();
         }
     }
 }
