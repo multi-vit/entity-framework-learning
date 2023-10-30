3e1d9382b972097bda3e2638cb2467bfdf46cf9c
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-10-30 08:54:47 +0000

    Add secrets for local DB userID and Password

--- EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
+++ EntityFrameworkNet5.Data/FootballLeagueDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using EntityFrameworkNet5.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +13,13 @@ namespace EntityFrameworkNet5.Data
 {
     public class FootballLeagueDbContext : DbContext
     {
+        private IConfigurationRoot config = new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+            .Build();
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=FootballLeague_EfCore")
+            optionsBuilder.UseSqlServer($"Data Source=localhost; Initial Catalog=FootballLeague_EfCore; User Id={config["UserId"]}; Password={config["Password"]}; TrustServerCertificate=True; Encrypt=True")
                 .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
