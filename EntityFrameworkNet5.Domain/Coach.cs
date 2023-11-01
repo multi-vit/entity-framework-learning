b22c6a491f937c4fb98ecaefc1c9ee77a7c5e91d
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-01 16:37:18 +0000

    Add coach domain model

---
+++ EntityFrameworkNet5.Domain/Coach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EntityFrameworkNet5.Domain.Common;
+
+namespace EntityFrameworkNet5.Domain
+{
+	public class Coach : BaseDomainObject
+	{
+		public string Name { get; set; }
+		public int? TeamId { get; set; }
+		public virtual Team Team { get; set; }
+	}
+}
+
