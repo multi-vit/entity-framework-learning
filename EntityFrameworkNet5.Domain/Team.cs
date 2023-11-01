d1356247ad855e2a511f1334608c6c5514ae4059
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-01 16:37:33 +0000

    Update Team domain model to include match and coach

--- EntityFrameworkNet5.Domain/Team.cs
+++ EntityFrameworkNet5.Domain/Team.cs
@@ -12,5 +12,8 @@ namespace EntityFrameworkNet5.Domain
         public string Name { get; set; }
         public int LeagueId { get; set; }
         public virtual League League { get; set; }
+        public virtual Coach Coach { get; set; }
+        public virtual List<Match> HomeMatches { get; set; }
+        public virtual List<Match> AwayMatches { get; set; }
     }
 }
