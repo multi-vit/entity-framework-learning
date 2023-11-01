f00757b17a36d34bded0fdc86243c40ea1867a69
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-01 16:37:10 +0000

    Add Match domain model

---
+++ EntityFrameworkNet5.Domain/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EntityFrameworkNet5.Domain.Common;
+
+namespace EntityFrameworkNet5.Domain
+{
+	public class Match : BaseDomainObject
+    {
+		public int HomeTeamId { get; set; }
+		public virtual Team HomeTeam { get; set; }
+		public int AwayTeamId { get; set; }
+		public virtual Team AwayTeam { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
+
