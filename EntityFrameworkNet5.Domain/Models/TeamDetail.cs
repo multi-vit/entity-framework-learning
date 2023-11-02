aad60229e6203f899a54837ef4e312348a40a76d
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-02 14:23:52 +0000

    Add TeamDetail data model

---
+++ EntityFrameworkNet5.Domain/Models/TeamDetail.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EntityFrameworkNet5.Domain.Models
+{
+	public class TeamDetail
+	{
+		public string Name { get; set; }
+		public string CoachName { get; set; }
+		public string LeagueName { get; set; }
+	}
+}
+
