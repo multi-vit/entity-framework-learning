40599bfe99d8bc96c5cbd75db608d236f2ed6eab
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-03 15:00:06 +0000

    Add DB migration for stored procedure

---
+++ EntityFrameworkNet5.Data/Migrations/20231103142419_AddingSPGetCoachName.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkNet5.Data.Migrations
+{
+    public partial class AddingSPGetCoachName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE PROCEDURE sp_GetTeamCoach
+                                    @teamId int
+                                    AS
+                                    BEGIN
+                                        SELECT * from Coaches where TeamId = @teamId
+                                    END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE [dbo].[sp_CoachName]");
+        }
+    }
+}
