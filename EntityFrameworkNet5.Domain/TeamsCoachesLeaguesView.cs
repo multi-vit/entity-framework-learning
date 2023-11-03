cc5a24d2439135fd7c6fcf43c3b37e4e39a27720
multi-vit <103585624+multi-vit@users.noreply.github.com>
2023-11-03 12:06:53 +0000

    Add TeamsCoachesLeagues view data model

---
+++ EntityFrameworkNet5.Domain/TeamsCoachesLeaguesView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EntityFrameworkNet5.Domain
+{
+	public class TeamsCoachesLeaguesView
+	{
+		public string Name { get; set; }
+		public string CoachName { get; set; }
+		public string LeagueName { get; set; }
+	}
+}
+
